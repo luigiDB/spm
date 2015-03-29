5a11ce89f662a7e3bacd26c6913df636ce445f9b
luigiDB <spada.elfica@gmail.com>
2015-03-29 16:44:14 +0200

    miglioramenti

--- background.js
+++ background.js
@@ -3,7 +3,9 @@ ALL' AVVIO DELL'APPLICAZIONE CONTROLLARE CHE SIA PRESENTE IL DATABASE
 SE NON PRESENTE CREALO
 */
 
+//scatta all'apertura della scheda
 chrome.tabs.onActivated.addListener(function (tabId, windowId){
+  console.log("onActivated()");
   chrome.tabs.query({active: true, windowId: windowId}, function(activeTabs) {
     var activeUrl = activeTabs[0].url;
     var startIndex = activeUrl.indexOf("//")+2;
@@ -13,17 +15,23 @@ chrome.tabs.onActivated.addListener(function (tabId, windowId){
     
     /*
     */
-    if(activeUrl.toLowercase()=="www.google.com"){
-      console.log("catch condition");
+    if(activeUrl=="www.google.it"){
+      console.log("catch condition a");
       chrome.browserAction.setIcon({path:"assets/icon_128_interrogativo.png", tabId:tabId.id});
-    }
+    } /*else {
+      chrome.browserAction.setIcon({path:"assets/icon_128.png", tabId:tabId.id});
+    }*/ //QUESTA NON HA SENSO DATO CHE QUESTO EVENTO LO VEDO SOLO NELLA ONUPDATED()
     
   });
 });
 
-
+//scatta quando cambio pagina all'interno di una scheda
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-  chrome.tabs.query({active: true}, function(activeTabs) {
-    console.log("changeInfo: " + changeInfo.url);
-  });
+  if(changeInfo.status=="complete"){
+    console.log("onUpdated() complete");
+    console.log("changeInfo URL: " + changeInfo.url);
+  } else {
+    console.log("onUpdated() loading");
+    console.log("changeInfo URL: " + changeInfo.url);
+  }
 });
