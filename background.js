786e295ea0bc7814cbbd5d3fa91b312342029aee
luigiDB <spada.elfica@gmail.com>
2015-03-29 12:31:57 +0200

    add onUpdate listener

--- background.js
+++ background.js
@@ -1,10 +1,3 @@
-/*add yhis to manifest if want a background js
-"background": {
-    "scripts": ["background.js"],
-    "persistent": false
-  }
-  
-*/
 /*
 ALL' AVVIO DELL'APPLICAZIONE CONTROLLARE CHE SIA PRESENTE IL DATABASE 
 SE NON PRESENTE CREALO
@@ -16,11 +9,21 @@ chrome.tabs.onActivated.addListener(function (tabId, windowId){
     var startIndex = activeUrl.indexOf("//")+2;
     var endIndex = activeUrl.indexOf("/", startIndex);
     activeUrl = activeUrl.substring(startIndex, endIndex);
-    console.log("new tab with url: "+activeUrl);
+    console.log("new tab with url: " + activeUrl);
     
     /*
     */
-    chrome.browserAction.setIcon({path:"assets/icon_128_interrogativo.png", tabId:tabId.id});
+    if(activeUrl.toLowercase()=="www.google.com"){
+      console.log("catch condition");
+      chrome.browserAction.setIcon({path:"assets/icon_128_interrogativo.png", tabId:tabId.id});
+    }
     
+  });
+});
+
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+  chrome.tabs.query({active: true}, function(activeTabs) {
+    console.log("changeInfo: " + changeInfo.url);
   });
 });
